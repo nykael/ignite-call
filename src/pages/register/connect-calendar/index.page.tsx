e48e5cbc24d3ed18f02ed3e8f1c3ce0485de4b75
nykael <nykaelccardial@gmail.com>
2023-01-27 15:24:45 -0300

    Prisma new tables

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,67 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { ArrowRight, Check } from 'phosphor-react'
+import { signIn, useSession } from 'next-auth/react'
+import { Container, Header } from '../styles'
+import { AuthError, ConnectBox, ConnectItem } from './styles'
+import { useRouter } from 'next/router'
+
+export default function Register() {
+  async function handleConnectCalendar() {
+    await signIn('google')
+  }
+
+  const session = useSession()
+  const router = useRouter()
+
+  console.log(session)
+
+  const hasAuthError = !!router.query.error
+  const isSignedIn = session.status === 'authenticated'
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda !</Heading>
+        <Text>
+          Conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida e que são agendados
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          {isSignedIn ? (
+            <Button variant="secondary" size="sm" disabled>
+              Conectado
+              <Check />
+            </Button>
+          ) : (
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={handleConnectCalendar}
+            >
+              Conectar
+              <ArrowRight />
+            </Button>
+          )}
+        </ConnectItem>
+
+        {hasAuthError && (
+          <AuthError size="sm">
+            Fala ao se conectar ao Google, verifique se você habilitou as
+            permissões de acesso ao Google Calendar
+          </AuthError>
+        )}
+
+        <Button type="submit" disabled={!isSignedIn}>
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
