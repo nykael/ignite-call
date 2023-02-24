c957c7667205883cc28353a586fbadb32464e766
nykael <nykaelccardial@gmail.com>
2023-02-24 17:51:24 -0300

    Scheduling

---
+++ src/components/Calendar/styles.ts
@@ -0,0 +1,22 @@
+import { styled, Text } from '@ignite-ui/react'
+
+export const CalendarContainer = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '$6',
+  padding: '$6',
+})
+
+export const CalendarHeader = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+})
+
+export const CalendarTitle = styled(Text, {})
+
+export const CalendarActions = styled('div', {})
+
+export const CalendarBody = styled('table', {})
+
+export const CalendarDay = styled('button', {})
