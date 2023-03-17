cf777f0e2c859d3659e8fdce1df3e88a92ac0943
nykael <nykaelccardial@gmail.com>
2023-03-17 15:16:16 -0300

    flow auth

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,6 +1,22 @@
+import { useState } from 'react'
 import { CalendarStep } from './CalendarStep'
-// import { ConfirmStep } from './ConfirmStep'
+import { ConfirmStep } from './ConfirmStep'
 
 export function ScheduleForm() {
-  return <CalendarStep />
+  const [selectedDateTime, setSelectedDate] = useState<Date | null>()
+
+  function handleClearSelectedDateTime() {
+    setSelectedDate(null)
+  }
+
+  if (selectedDateTime) {
+    return (
+      <ConfirmStep
+        onCancelConfirmation={handleClearSelectedDateTime}
+        schedulingDate={selectedDateTime}
+      />
+    )
+  }
+
+  return <CalendarStep onSelectDateTime={setSelectedDate} />
 }
